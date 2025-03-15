79d84260c8a1773f15431bf0b1ef8409e9567f60
hyeongu01 <cdy5212@gmail.com>
2025-03-15 18:44:26 +0900

    -

---
+++ solving/1476.cpp
@@ -0,0 +1,28 @@
+/*
+    https://www.acmicpc.net/problem/1476
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int e, s, m;
+    cin >> e >> s >> m;
+    e --;
+    s --;
+    m --;
+    int result = e;
+
+    while(true) {
+        if (result % 28 == s) break;
+        result += 15;
+    }
+    while (true) {
+        if (result % 19 == m) break;
+        result += 15*28;
+    }
+    cout << result + 1 << endl;
+
+    return 0;
+}
