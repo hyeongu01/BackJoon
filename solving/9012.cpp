799965d51058a405045d0819424ebae4dbcf2bbd
hyeongu01 <cdy5212@gmail.com>
2025-04-08 01:50:11 +0900

    Update 9012.cpp

--- solving/9012.cpp
+++ solving/9012.cpp
@@ -4,11 +4,43 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+
+bool checkPars(std::string inputPars) {
+    std::vector<char> pars;
+
+    if (inputPars.back() == '(')
+        return false;
+
+    while (inputPars.empty() != true) {
+        char temp = inputPars.back();
+        inputPars.pop_back();
+        if (temp == ')') {
+            pars.push_back(temp);
+        } else {
+            if (pars.back() == ')')
+                pars.pop_back();
+            else
+                return false;
+        }
+    }
+    if (pars.empty() == true)
+        return true;
+    else
+        return false;
+}
 
 int main() {
     int T;
     std::cin >> T;
-    
+    std::string result;
+
+    for (int i = 0; i < T; i++) {
+        std::string inputPars;
+        std::cin >> inputPars;
+        result += (checkPars(inputPars) ? "YES\n" : "No\n");
+    }
+    std::cout << result;
 
     return 0;
 }
